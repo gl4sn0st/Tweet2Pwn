fa96844440da8ad6da79588c4f2a9a468d057987
gl4sn0st <glasnost@deicide.pl>
2021-10-26 13:43:54 +0200

    env

--- t2p.py
+++ t2p.py
@@ -1,7 +1,12 @@
 from TwitterAPI import TwitterAPI
 import json
+import os
 
-api = TwitterAPI("ypR4UyFZwOExNuSHk5yY1HBxL", "imE9YwcSIQDVnaf4VGWlfm0zT0191wnME07lPm42uQ6UkPRdpu", "1452730066190876679-mfGkF2fQAZpZAB95EavXDQVfh4jhGe", "lkldhBWdxcZ6UZVeSx2ohpvLTzjQwCJG0JYHFheLc1bsH")
+consumer = os.environ['CONSUMER_KEY']
+consumer_secret = os.environ['CONSUMER_SECRET']
+access = os.environ['ACCESS_TOKEN']
+secret = os.environ['SECRET_TOKEN']
+api = TwitterAPI(consumer, consumer_secret, access, secret)
 
 r = api.request("direct_messages/events/list")
 print(r.text)
